318a4dfe99264dd404ec5a447981167fc9192951
Akatsuki <33372019+raaapiw@users.noreply.github.com>
2020-04-16 21:56:00 +0700

    update bugs

--- resources/views/pages/cart/index.blade.php
+++ resources/views/pages/cart/index.blade.php
@@ -66,7 +66,7 @@
                                 <form action="{{ route('tambahOrder', Sentinel::getUser()->id) }}" method="POST" id="addOrder">
                                     <div class="form-group">
                                         <label for="exampleInputEmail1">Brief Url</label>
-                                        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Brief URL">
+                                        <input type="text" name="brief" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Brief URL">
                                         <small id="emailHelp" class="form-text text-muted">Put Your brief on GDrive, and copy your brief link here!</small>
                                     </div>
                                     @foreach ($carts as $row)
--- resources/views/pages/product/selectCategory.blade.php
+++ resources/views/pages/product/selectCategory.blade.php
@@ -22,36 +22,41 @@
 <div class="content-wrap">
 
     <div class="container clearfix">
-        
-            <div class="col-lg-12 text-center">
-                <h2>Order Information</h2>
-                <h4>Choose category for your product</h4>
+
+        <div class="col-lg-12 text-center">
+            <h2>Order Information</h2>
+            <h4>Choose category for your product</h4>
+        </div>
+        <div class="feature-box fbox-center fbox-effect nobottomborder" >
+            <div class="fbox-icon">
+                <i class="icon-box"></i>
             </div>
-            <div class="feature-box fbox-center fbox-effect nobottomborder" >
-                <div class="fbox-icon">
-                    <i class="icon-box"></i>
+            @if ($product->category_id == 1)
+            <h3>Desain Produksi</h3>
+            @elseif ($product->category_id == 2)
+            <h3>Desain Non Produksi</h3>
+            @else
+            <h3>Video</h3>
+            @endif
+        </div>
+
+        <div class="">
+            <form method="" class="row center my-auto">
+
+                <div class="col-lg-12 category-form form-group">
+                    <label for="exampleFormControlSelect1">Select category</label>
+                    <select class="form-control " id="exampleFormControlSelect1">
+                        @foreach($products as $row)
+                        <option value="{{$row->id}}">{{$row->value}}</option>
+                        @endforeach
+                    </select>
                 </div>
-                <h3>Desain Produksi</h3>
-            </div>
-    
-            <div class="">
-                <form method="" class="row center my-auto">
-                    
-                    <div class="col-lg-12 category-form form-group">
-                        <label for="exampleFormControlSelect1">Select category</label>
-                        <select class="form-control " id="exampleFormControlSelect1">
-                            <option>Banner</option>
-                            <option>Baliho</option>
-                            <option>Poster</option>
-                        </select>
-                    </div>
-                    <div class="col-lg-12 form-group">
-                        <a href="" class="btn btn-primary" onclick="parentNode.submit()">Next Step</a>
-                    </div>
-                </form>
-            </div>
-        
-        
+                <div class="col-lg-12 form-group">
+                    <a href="{{ route('productOrder', 1)}}" class="btn btn-primary" onclick="parentNode.submit()">Next Step</a>
+                </div>
+                {{-- @endforeach --}}
+            </form>
+        </div>
     </div>
 </div>
 
