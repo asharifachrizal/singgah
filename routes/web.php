81dda5ab1f98cb888613b910ddd187e610e0732c
januarelsan <januarelsan@gmail.com>
2020-04-16 17:42:36 +0700

    Delete Unused Route

--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,11 @@
 |
 */
 
+
 Route::view('/', 'pages.home')->name('home');
 Route::view('/faq', 'pages.faq')->name('faq');
 Route::view('/about-us', 'pages.about-us')->name('aboutUs');
 
-// order cart & cart pages
-// Route::view('/order/detail', 'pages.cart.order-detail')->name('order.detail');
 Route::view('/order/detail/invoice', 'pages.cart.order-invoice')->name('order.detail.invoice');
 
 
@@ -38,22 +37,10 @@ Route::group(['middleware' => 'guest'], function() {
     Route::view('/profile', 'pages.profile')->name('profile');
 });
 
-Route::get('/kategori', 'CategoryController@index')->name('category');
-Route::get('/kategori/{slug}', 'CategoryController@detail')->name('category.detail');
-
-// route baru for 
-Route::view('/product/select-product', 'pages.product.selectCategory')->name('forSelectCategory');
-Route::view('/product/my-order', 'pages.product.myOrder')->name('forMyOrder');
-
-Route::get('/product', 'ProductController@index')->name('product');
-Route::get('/product/category', 'ProductController@selectCategory')->name('selectCategory');
-Route::get('/product/order', 'ProductController@order')->name('productOrder');
-// Route::get('/product/select-product/{slug}/{category_id}', 'ProductController@selectProduct')->name('product.select');
-Route::get('/product/detail', 'ProductController@detailProduct')->name('product.detail');
 
 Route::group(['middleware' => 'visitor'], function() {
     Route::post('/logout', 'UserController@postLogout')->name('postLogout');
-
+    Route::get('/category', 'ProductController@index')->name('product');
     Route::get('/cart', 'CartController@cartClient')->name('cartUser');
     Route::get('/keranjang', 'CartController@index')->name('cart');
     Route::get('/keranjang/pembayaran', 'CartController@checkout')->name('cart.checkout');
@@ -65,4 +52,8 @@ Route::group(['middleware' => 'visitor'], function() {
     Route::post('/order/{id}', 'CartController@addOrder')->name('tambahOrder');
     Route::get('/my-order/{id}', 'CartController@myOrder')->name('myorder');
     Route::get('/my-order/detail/{id}', 'CartController@detailOrder')->name('detailOrder');
+
+    //Punya Akbar
+    Route::view('/product/select-product', 'pages.product.selectCategory')->name('forSelectCategory');
+    Route::view('/product/my-order', 'pages.product.myOrder')->name('forMyOrder');
 });
