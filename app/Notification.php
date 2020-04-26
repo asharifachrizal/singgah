7b0f8b0c8d3784b57823814b81fdcb8169a41ea3
januarelsan <januarelsan@gmail.com>
2020-04-27 00:44:10 +0700

    Generate Notification Model, Migration, Controller File

---
+++ app/Notification.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Notification extends Model
+{
+    //
+    protected $fillable = [
+        'user_id',
+        'notif_type_id',
+        'title',
+        'value',                
+        'status',        
+        
+
+    ];
+
+    public function user()
+	{
+		return $this->belongsTo('App\User');
+    }
+    public function notifType()
+	{
+		return $this->belongsTo('App\NotifType');
+    }
+}
