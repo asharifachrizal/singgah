bf2f139d8a929ea283111e12c9769ea46e6819e2
irfan <m.irfan0081@gmail.com>
2020-04-27 01:02:17 +0700

    input fields  audience n color already dynamically

--- resources/views/pages/product/myOrder.blade.php
+++ resources/views/pages/product/myOrder.blade.php
@@ -61,16 +61,16 @@
                                     <input type="hidden" name="product_id" value="{{ $product->id }}">                                    
                                     <div class="form-process"></div>
                                     <div class="col-12 form-group">
-                                        <label for="jenis-produk">Jenis Produk:</label>
+                                        <label for="jenis-produk">PRODUCT</label>
                                         <select disabled class="form-control required" >
                                                 <option value="{{$product->value}}">{{$product->value}}</option>                                                
                                         </select>
                                     </div>
                                     <div class="col-6 form-group">
-                                        <label for="kuatiti-produk">Kuantiti:</label><br>
+                                        <label for="kuantitas-produk">Qty</label><br>
                                         <input required type="number" name="quantity" id="quantity" class="form-control required" value="1" placeholder="minimal 1" min="1">
                                     </div>
-                                    @if($product->id == 3 || $product->id == 5 || $product->id == 7 )
+                                    
                                     <div class="col-6 form-group">
                                         <label for="deadline-produk">Orientasi:</label><br>                                        
                                         <select required class="form-control required" name="orientation" id="orientation" onchange="orientationSelected(this.value)">
@@ -79,11 +79,7 @@
                                             <option value="horizontal">Horizontal</option>                                                
                                         </select>
                                     </div> 
-                                    @else
-                                        <select hidden required class="form-control required" name="orientation_" id="orientation_" onchange="orientationSelected(this.value)">
-                                            <option value="null" selected disabled></option>                                            
-                                        </select>
-                                    @endif  
+                                    
                                     @if($product->id > 0 && $product->id < 16 )                                
                                     <div class="col-6 form-group">
                                         <label for="ukuran-rasio-produk">Ukuran/rasio:</label><br>
@@ -97,8 +93,23 @@
                                     </div>    
                                     @endif 
                                     <div class="col-12 form-group">
-                                        <label for="target-audience-produk">Target Audience:</label><br>
-                                        <input type="text" required name="target_audience" id="target_audience" class="form-control required" value="" placeholder="anak-anak-dewasa / Umur 24-30">
+                                        <label for="target-audience-produk">Target Audience</label><br>
+                                        <div class="row" id="inputAudience-Warp">
+                                            <div class="col-lg-12 col-md-12" id="warpAudience_0">
+                                                <div class="input-group bootstrap-touchspin">
+                                                    <input id="indexAudience_0"  type="text" placeholder="anak-anak-dewasa / Umur 24-30" data-bts-button-down-class="btn btn-secondary btn-outline" data-bts-button-up-class="btn btn-secondary btn-outline" class="form-control" style="display: block;">
+                                                    <div class="input-group-append bootstrap-touchspin-postfix" style="display: none;">
+                                                        <span class="input-group-text"></span>
+                                                    </div>
+                                                    <div class="input-group-append" id="spliceAudience_0" hidden="true">
+                                                        <button class="btn btn-danger bootstrap-touchspin-up" type="button" onclick="spliceAudience(0)">-</button>
+                                                    </div>
+                                                    <div class="input-group-append">
+                                                        <button class="btn btn-primary bootstrap-touchspin-up" type="button" onclick="addMoreAudience()">+</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </div>   
                                     @if($product->id <= 15)                                                                                                                                                                             
                                     <div class="col-12 form-group">
@@ -135,8 +146,6 @@
                                             </div>
                                         </div>
                                     </div>
-                                    
-                            
                                     <div class="col-12 form-group">
                                         <label for="design-style-produk">Font</label>
                                         <input type="text" required name="font" id="font" class="form-control required" value="" placeholder="Arial, Calibri">
@@ -153,37 +162,6 @@
                                             <option value="normal">Nomral</option>
                                         </select>
                                     </div>
-                                    <!-- <div class="col-lg-12 bottommargin">
-                                        <label>Select Multiple Items:</label><br>
-                                        <div class="file-input file-input-new"><div class="file-preview ">
-                                            <button type="button" class="close fileinput-remove" aria-label="Close">
-                                                <span aria-hidden="true">×</span>
-                                            </button>    
-                                            <div class="file-drop-disabled">
-                                                <div class="file-preview-thumbnails">
-                                            </div>
-                                            <div class="clearfix"></div>    <div class="file-preview-status text-center text-success"></div>
-                                            <div class="kv-fileinput-error file-error-message" style="display: none;"></div>
-                                            </div>
-                                        </div>
-                                        <div class="kv-upload-progress kv-hidden" style="display: none;"><div class="progress">
-                                            <div class="progress-bar bg-success progress-bar-success progress-bar-striped active" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width:0%;">
-                                                0%
-                                            </div>
-                                        </div></div><div class="clearfix"></div>
-                                        <div class="input-group file-caption-main">
-                                        <div class="file-caption form-control kv-fileinput-caption" tabindex="500">
-                                        <span class="file-caption-icon"></span>
-                                        <input class="file-caption-name" onkeydown="return false;" onpaste="return false;" placeholder="Select files...">
-                                        </div>
-                                        <div class="input-group-btn input-group-append">
-                                            <button type="button" tabindex="500" title="Clear selected files" class="btn btn-default btn-secondary fileinput-remove fileinput-remove-button"><i class="glyphicon glyphicon-trash"></i>  <span class="d-none d-sm-inline-block">Remove</span></button>
-                                            <button type="button" tabindex="500" title="Abort ongoing upload" class="btn btn-default btn-secondary kv-hidden fileinput-cancel fileinput-cancel-button"><i class="glyphicon glyphicon-ban-circle"></i>  <span class="d-none d-sm-inline-block">Cancel</span></button>
-                                            
-                                            <div tabindex="500" class="btn btn-primary btn-file"><i class="glyphicon glyphicon-folder-open"></i>&nbsp;  <span class="d-none d-sm-inline-block">Browse …</span><input id="input-3" name="input2[]" type="file" class="file" multiple="" data-show-upload="false" data-show-caption="true" data-show-preview="true"></div>
-                                            </div>
-                                        </div></div>
-                                    </div> -->
                                     <div class="col-12 hidden">
                                         <input type="text" id="event-registration-botcheck" name="event-registration-botcheck" value="" />
                                     </div>
@@ -214,19 +192,15 @@
 <script src="{{ asset('material/plugins/jquery-asColorPicker-master/libs/jquery-asGradient.js') }}"></script>
 <script src="{{ asset('material/plugins/jquery-asColorPicker-master/dist/jquery-asColorPicker.min.js') }}"></script>
 <script>
-    var x = 1
-    var counterWarp_Color = [0]
-    var warpInput_Color = $("#inputColor-Warp")
+    // VARIABLE COLOR PICKER
+    var xColor = 1 //INDEXING COLOR
+    var counterWarp_Color = [0] //INDEX STORAGE COLOR
+    var warpInput_Color = $("#inputColor-Warp") //WARP INPUT COLOR PICKER
     
-    
-    var deadlines
-    var chooseColor = [
-        {
-            index: 0,
-            color: '#ffffff'
-        }
-    ]
-
+    // START FUNC COLOR PICKER
+    /**
+     * @params warpNumber: warp number
+     */
     async function spliceColor( warpNumber ) {
         if(counterWarp_Color.length > 1) {
             $(`#warpColor_${warpNumber}`).remove()
@@ -238,15 +212,26 @@
         }
     }
 
+    /**
+     * @params warpNumber: number warp
+     */
     function openColorPicker(warpNumber) {
         $(`#colorPicker_${warpNumber}`).click()
     }
 
+    /**
+     * @params warpNumber: number warp
+     * @params data: input value
+     */
     function changeColor_Picker(data, warpNumber) {
         $(`#indexColor_${warpNumber}`).val(data)
         $(`#btnPicker_${warpNumber}`).css('background-color', `${data}`)
     }
 
+    /**
+     * @params warpNumber: number warp
+     * @params data: input value
+     */
     function changeColor_Input( data, warpNumber ) {
         $(`#colorPicker_${warpNumber}`).val(data)
         $(`#btnPicker_${warpNumber}`).css('background-color', `${data}`)
@@ -254,21 +239,21 @@
 
     function addMoreColor() {
         warpInput_Color.append(`
-        <div class="col-lg-12 col-md-12" id="warpColor_${x}" style="margin-top: 10px">
-            <input type="color" id="colorPicker_${x}" value="#acacec" hidden onchange="changeColor_Picker(this.value, ${x})">
+        <div class="col-lg-12 col-md-12" id="warpColor_${xColor}" style="margin-top: 10px">
+            <input type="color" id="colorPicker_${xColor}" value="#acacec" hidden onchange="changeColor_Picker(this.value, ${xColor})">
             <div class="input-group bootstrap-touchspin">
                 <div class="input-group-prepend">
-                    <button id="btnPicker_${x}" onclick="openColorPicker(${x})" class="btn btn-secondary btn-outline bootstrap-touchspin-down" type="button" style="background-color: #acacec; width: 40px;"> </button>
+                    <button id="btnPicker_${xColor}" onclick="openColorPicker(${xColor})" class="btn btn-secondary btn-outline bootstrap-touchspin-down" type="button" style="background-color: #acacec; width: 40px;"> </button>
                 </div>
                 <div class="input-group-prepend bootstrap-touchspin-prefix" style="display: none;">
                     <span class="input-group-text"></span>
                 </div>
-                <input id="indexColor_${x}" onchange="changeColor_Input(this.value, ${x})" type="text" value="#acacec" name="tch3" data-bts-button-down-class="btn btn-secondary btn-outline" data-bts-button-up-class="btn btn-secondary btn-outline" class="form-control" style="display: block;">
+                <input id="indexColor_${xColor}" onchange="changeColor_Input(this.value, ${xColor})" type="text" value="#acacec" name="tch3" data-bts-button-down-class="btn btn-secondary btn-outline" data-bts-button-up-class="btn btn-secondary btn-outline" class="form-control" style="display: block;">
                 <div class="input-group-append bootstrap-touchspin-postfix" style="display: none;">
                     <span class="input-group-text"></span>
                 </div>
-                <div class="input-group-append" id="spliceColor_${x}" >
-                    <button class="btn btn-danger bootstrap-touchspin-up" type="button" onclick="spliceColor(${x})">-</button>
+                <div class="input-group-append" id="spliceColor_${xColor}" >
+                    <button class="btn btn-danger bootstrap-touchspin-up" type="button" onclick="spliceColor(${xColor})">-</button>
                 </div>
                 <div class="input-group-append">
                     <button class="btn btn-primary bootstrap-touchspin-up" type="button" onclick="addMoreColor()">+</button>
@@ -279,10 +264,52 @@
         if(counterWarp_Color.length == 1) {
             $(`#spliceColor_${counterWarp_Color[0]}`).removeAttr('hidden')
         }
-        counterWarp_Color.push(x)
-        x++;
+        counterWarp_Color.push(xColor)
+        xColor++;
+    }
+    // END FUNC COLOR
+    
+    // VARIABLE AUDIENCE
+    var xAudience = 1 //INDEXING INPUT AUDIENCE
+    var counterWarp_Audience = [0] //INDEX STORAGE AUDIENCE
+    var warpInput_Audience = $("#inputAudience-Warp") //WARP INPUT AUDIENCE
+
+    // START FUNC AUDIENCE
+    function addMoreAudience() {
+        warpInput_Audience.append(`
+        <div class="col-lg-12 col-md-12" id="warpAudience_${xAudience}" style="margin-top: 10px">
+            <div class="input-group bootstrap-touchspin">
+                <input id="indexAudience_${xAudience}"  type="text" placeholder="anak-anak-dewasa / Umur 24-30" data-bts-button-down-class="btn btn-secondary btn-outline" data-bts-button-up-class="btn btn-secondary btn-outline" class="form-control" style="display: block;">
+                <div class="input-group-append bootstrap-touchspin-postfix" style="display: none;">
+                    <span class="input-group-text"></span>
+                </div>
+                <div class="input-group-append" id="spliceAudience_${xAudience}" >
+                    <button class="btn btn-danger bootstrap-touchspin-up" type="button" onclick="spliceAudience(${xAudience})">-</button>
+                </div>
+                <div class="input-group-append">
+                    <button class="btn btn-primary bootstrap-touchspin-up" type="button" onclick="addMoreAudience()">+</button>
+                </div>
+            </div>
+        </div>`)
+        if(counterWarp_Audience.length == 1) {
+            $(`#spliceAudience_${counterWarp_Audience[0]}`).removeAttr('hidden')
+        }
+        counterWarp_Audience.push(xAudience)
+        xAudience++;
     }
 
+    async function spliceAudience( warpNumber ) {
+        if(counterWarp_Audience.length > 1) {
+            $(`#warpAudience_${warpNumber}`).remove()
+            await counterWarp_Audience.splice(counterWarp_Audience.findIndex( _warpNumber => _warpNumber == warpNumber ), 1)
+            if(counterWarp_Audience.length == 1) {
+                $(`#spliceAudience_${counterWarp_Audience[0]}`).attr("hidden", true);
+            }
+        }
+    }
+    // END FUNC AUDIENCE
+
+    var deadlines
     function deadlineSelected(data) {
         deadlines = data
         console.log('deadline ', data)
@@ -301,7 +328,6 @@
         var duration = $("input[name=duration]").val();
         var target_audience = $("input[name=target_audience]").val();                
         var style = $("input[name=style]").val();
-        var color = $("input[name=color]").val();
         var color_grading = $("input[name=color_grading]").val();
         var font = $("input[name=font]").val();        
         var output = $("input[name=output]").val();        
